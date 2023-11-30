33ec7809c2f6cc5018f2971bb9b39e44daa52349
AMY ANDREW <AMYRTANDREW@GMAIL.COM>
2023-11-30 10:19:19 -0700

    login page css

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -6,37 +6,80 @@ export default function LoginForm({ onLogin }) {
   const [passwordValue, setPasswordValue] = useState("");
 
   return (
-    <form
-      className="login-form"
-      onSubmit={(e) => {
-        onLogin(e, {
-          username: usernameValue,
-          password: passwordValue,
-        });
+    <div
+      style={{
+        backgroundColor: "white",
+        padding: "60px",
+        marginTop: "15vh",
+        marginLeft: "25vw",
+        marginRight: "25vw",
+        borderRadius: "10px",
+        height: "45vh",
+        // alignContent: "center",
       }}
     >
-      <label htmlFor="username">Username:</label>
-      <input
-        name="username"
-        id="username"
-        type="text"
-        placeholder="username"
-        required
-        onChange={(e) => setUsernameValue(e.target.value)}
-      />
-      <label htmlFor="password">Password:</label>
-      <input
-        name="password"
-        id="password"
-        type="password"
-        placeholder="password"
-        required
-        onChange={(e) => setPasswordValue(e.target.value)}
-      />
-      <button type="submit">Log In</button>
-      <p>
-        Don't have an account? <Link to="/create-account">Create Account</Link>{" "}
-      </p>
-    </form>
+      <form
+        className="login-form"
+        onSubmit={(e) => {
+          onLogin(e, {
+            username: usernameValue,
+            password: passwordValue,
+          });
+        }}
+      >
+        <input
+          id="username"
+          type="text"
+          placeholder="username"
+          required
+          className="user-input"
+          style={{ borderRadius: "10px", width: "18vw" }}
+          onChange={(e) => setUsernameValue(e.target.value)}
+        />
+        <input
+          name="password"
+          id="password"
+          type="password"
+          placeholder="password"
+          required
+          className="user-input"
+          style={{ borderRadius: "10px", width: "18vw", marginTop: "20px" }}
+          onChange={(e) => setPasswordValue(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="login-btn"
+          style={{
+            backgroundColor: "royalblue",
+            color: "white",
+            marginLeft: "auto",
+            marginRight: "auto",
+            position: "relative",
+            display: "flex",
+            marginTop: "10px",
+            width: "18vw",
+            textAlign: "center",
+          }}
+        >
+          <p style={{ marginLeft: "auto", marginRight: "auto" }}>Log In</p>
+        </button>
+      </form>
+      <span
+        style={{
+          position: "relative",
+          display: "flex",
+          width: "fit-content",
+          textAlign: "center",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "10px",
+        }}
+      >
+        Don't have an account?{" "}
+        <span style={{ textDecoration: "underline" }}>
+          <Link to="/create-account">Create Account</Link>
+        </span>
+      </span>
+    </div>
   );
 }
